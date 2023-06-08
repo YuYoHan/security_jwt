48ce3b802f20bb61835be0bc72852105faa0a2a9
YuYoHan <zxzz8014@naver.com>
2023-06-08 13:31:37 +0900

    [Study] OAuth2 공부

--- src/main/java/com/example/security_jwt/config/SecurityConfig.java
+++ src/main/java/com/example/security_jwt/config/SecurityConfig.java
@@ -1,13 +1,12 @@
 package com.example.security_jwt.config;
 
 import com.example.security_jwt.config.oauth.PrincipalOauth2UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /*
@@ -25,10 +24,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 // secured 어노테이션 활성화, preAuthorize 어노테이션 활성화
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Autowired
-    private PrincipalOauth2UserService principalOauth2UserService;
+    private final PrincipalOauth2UserService principalOauth2UserService;
 
 
 //    // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
